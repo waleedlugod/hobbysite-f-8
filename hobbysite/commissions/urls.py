4d1773da39faaada129846f0e902abf81cb528b1
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-05 14:35:38 +0800

    Setup commissions detail view

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import commission_list
+from .views import commission_detail, commission_list
 
 urlpatterns = [
     path("list", commission_list, name="commission_list"),
+    path("detail/<int:pk>", commission_detail, name="commission_detail"),
 ]
 
 app_name = "ledger"
--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -1,8 +1,12 @@
+from commissions.models import Commission
 from django.shortcuts import render
-
-from commissions.models import Comment, Commission
 
 
 def commission_list(request):
     commission_list = {"commission_list": Commission.objects.all()}
     return render(request, "commission_list.html", commission_list)
+
+
+def commission_detail(request, pk):
+    commission_detail = {"commission_detail": Commission.objects.get(pk=pk)}
+    return render(request, "commission_detail.html", commission_detail)
