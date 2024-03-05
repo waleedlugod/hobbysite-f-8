9e720f4d64fdf2e725eca05e193e18a1ee028f68
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-05 12:50:43 +0800

    Created models for Commission and Comment

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField()
+    updated_on = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-created_on"]
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        to=Commission, on_delete=models.CASCADE, related_name="comments"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField()
+    updated_on = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-created_on"]
