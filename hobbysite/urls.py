d7c423df94b5eb5456d5fc0acf9f93250619d5ae
Orangguu <jklwarain@gmail.com>
2024-03-16 23:47:35 +0800

    updated views.py and updated hobbysite urls to include wiki.urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("")]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("wiki.urls", namespace="wiki")),
+]
--- wiki/views.py
+++ wiki/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from .models import Article
 
 
-def article_list(request):
+def article_list_view(request):
     articles = Article.objects.all()
     ctx = {"articles": articles}
-    return render(request, "list_view.html", ctx)
+    return render(request, "article_list_view.html", ctx)
 
 
-def article_detail(request):
+def article_detail_view(request):
     ctx = {"article", Article.objects.get(id=id)}
-    return render(request, "detail_view.html", ctx)
+    return render(request, "article_detail_view.html", ctx)
 
 
 # Create your views here.
