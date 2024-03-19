8bb7d05d64d0cc5ac68e14ff3cbf185ba2bca064
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-19 21:29:06 +0800

    Moved templates to commission directory

--- commissions/views.py
+++ commissions/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 def commission_list(request):
     commission_list = {"commission_list": Commission.objects.all()}
-    return render(request, "commission_list.html", commission_list)
+    return render(request, "commission/commission_list.html", commission_list)
 
 
 def commission_detail(request, pk):
     commission_detail = {"commission_detail": Commission.objects.get(pk=pk)}
-    return render(request, "commission_detail.html", commission_detail)
+    return render(request, "commission/commission_detail.html", commission_detail)
