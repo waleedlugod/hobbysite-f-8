cb3830f8dce8d1b23670cfa13c970534fa6903ca
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-19 22:48:45 +0800

    Created list and detail views.

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product, ProductType
+
+def merch_list_view(request):
+    types = ProductType.objects.all()
+    ctx = {'types': types}
+
+    return render(request, 'product_list.html', ctx)
+
+def merch_detail_view(request, pk):
+    product = Product.objects.get(pk=pk)
+    ctx = {'product': product}
+
+    return render(request, 'product_detail.html', ctx)
