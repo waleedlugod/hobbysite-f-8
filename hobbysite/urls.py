7348d881a388c76131b146c2168b9f9dce533eef
Waleed Lugod <lugodwaleed@gmail.com>
2024-03-20 00:42:11 +0800

    Refactored urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("wiki.urls", namespace="wiki")),
+    path("wiki/", include("wiki.urls", namespace="wiki")),
 ]
--- wiki/urls.py
+++ wiki/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from .views import article_list_view, article_detail_view
 
 urlpatterns = [
-    path("wiki/articles", article_list_view, name="article-list"),
-    path("wiki/article/<int:pk>", article_detail_view, name="article-detail"),
+    path("articles", article_list_view, name="article-list"),
+    path("article/<int:pk>", article_detail_view, name="article-detail"),
 ]
 
 app_name = "wiki"
