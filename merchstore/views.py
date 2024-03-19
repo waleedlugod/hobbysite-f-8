63f5a2785e87d866e3a9ca658b8e6446d678b35f
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-03-19 23:58:45 +0800

    Added base template and edited templates and views.

--- merchstore/views.py
+++ merchstore/views.py
@@ -6,10 +6,10 @@ def merch_list_view(request):
     types = ProductType.objects.all()
     ctx = {'types': types}
 
-    return render(request, 'product_list.html', ctx)
+    return render(request, 'merchstore/product_list.html', ctx)
 
 def merch_detail_view(request, pk):
     product = Product.objects.get(pk=pk)
     ctx = {'product': product}
 
-    return render(request, 'product_detail.html', ctx)
+    return render(request, 'merchstore/product_detail.html', ctx)
