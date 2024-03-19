abc98b714103db967b30e1979ec6ef4564c6fd22
Joseph Sagusay <joseph.sagusay@student.ateneo.edu>
2024-03-19 21:03:30 +0800

    Added created_on and updated_on in blog_detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
 
 from .models import Article, ArticleCategory
 
