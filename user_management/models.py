032b7a24875d5eda0cac771b89d3a511e594aaea
Waleed Lugod <lugodwaleed@gmail.com>
2024-05-01 00:05:45 +0800

    Sort imports and changed verbose name for username

--- user_management/models.py
+++ user_management/models.py
@@ -1,7 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=63)
+    username = models.CharField(max_length=63, verbose_name="Display name")
