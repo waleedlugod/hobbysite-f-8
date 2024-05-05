239c73ec57d43eb25b086c27f4d12b95419ead6a
Waleed Lugod <lugodwaleed@gmail.com>
2024-05-05 23:45:37 +0800

    Changed models but without proper ordering

--- commissions/models.py
+++ commissions/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
+
 
 class Commission(models.Model):
     STATUS_CHOICES = {
@@ -11,7 +13,9 @@ class Commission(models.Model):
     }
     title = models.CharField(max_length=255)
     description = models.TextField()
-    status = models.CharField(max_length=255, default=STATUS_CHOICES["O"], choices=STATUS_CHOICES)
+    status = models.CharField(
+        max_length=255, default=STATUS_CHOICES["O"], choices=STATUS_CHOICES
+    )
     people_required = models.IntegerField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -32,11 +36,36 @@ class Job(models.Model):
         "F": "Full",
     }
     commission = models.ForeignKey(
-        to=Commission, on_delete=models.CASCADE, related_name="Job"
+        to=Commission, on_delete=models.CASCADE, related_name="job"
     )
     role = models.CharField(max_length=255)
-    status = models.CharField(max_length=255, default=STATUS_CHOICES["O"], choices=STATUS_CHOICES)
+    status = models.CharField(
+        max_length=255, default=STATUS_CHOICES["O"], choices=STATUS_CHOICES
+    )
     people_required = models.IntegerField()
 
     class Meta:
+        # TODO: make ordering better
         ordering = ["-status", "-people_required", "role"]
+
+
+class JobApplication(models.Model):
+    STATUS_CHOICES = {
+        "P": "Pending",
+        "A": "Accepted",
+        "R": "Rejected",
+    }
+    job = models.ForeignKey(
+        to=Job, on_delete=models.CASCADE, related_name="job_application"
+    )
+    applicant = models.ForeignKey(
+        to=Profile, on_delete=models.CASCADE, related_name="job_application"
+    )
+    status = models.CharField(
+        max_length=255, default=STATUS_CHOICES["P"], choices=STATUS_CHOICES
+    )
+    applied_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # TODO: fix ordering
+        ordering = ["-applied_on"]
