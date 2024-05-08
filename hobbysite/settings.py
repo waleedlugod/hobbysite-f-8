f3c034774c915c7a5ffc818b687614a91e834cc5
Waleed Lugod <lugodwaleed@gmail.com>
2024-05-08 19:50:50 +0800

    Add postgresql

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -84,11 +84,16 @@ WSGI_APPLICATION = "hobbysite.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
+
 
 
 # Password validation
