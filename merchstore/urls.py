99aef8926423fe44e307aa88fa28ffe788d197e5
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-05-08 17:42:01 +0800

    Edited urls.

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 
-from .views import merch_list_view, merch_detail_view
+from .views import product_list, product_detail, product_create, product_update, cart_list, transaction_list
 
 
 urlpatterns = [
-    path('items', merch_list_view, name = 'list'),
-    path('item/<int:pk>', merch_detail_view, name = 'detail')
+    path('items', product_list, name = 'list'),
+    path('item/<int:pk>', product_detail, name = 'detail'),
+    path("item/add", product_create, name = "product_create"),
+    path("item/<int:pk>/edit", product_update, name = "product_edit"),
+    path("cart", cart_list, name="cart"),
+    path("transactions", transaction_list, name="transaction"),
 ]
 
 app_name = "merchstore"
