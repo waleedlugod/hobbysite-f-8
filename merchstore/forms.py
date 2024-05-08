d3b06659fb3c3fc089a51257efc25340e42d70d9
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-05-08 17:41:37 +0800

    Created forms.

--- merchstore/forms.py
+++ merchstore/forms.py
@@ -3,13 +3,13 @@ from django import forms
 from .models import Product, Transaction
 
 
-class ProductForm(forms.Form):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ["name", "type", "description", "price", "stock", "status"]
 
 
-class TransactionForm(forms.Form):
+class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = "__all__"
+        fields = ["amount"]
