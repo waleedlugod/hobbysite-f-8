74f66a64223bf8bf3a33e99c377c38aee3254e13
Nina Sandejas <nina.sandejas@student.ateneo.edu>
2024-05-09 22:04:19 +0800

    Edited forms

--- merchstore/forms.py
+++ merchstore/forms.py
@@ -10,6 +10,19 @@ class ProductForm(forms.ModelForm):
 
 
 class TransactionForm(forms.ModelForm):
+    def __init__(self, *args, product=None, **kwargs):
+        self.product = product
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Transaction
         fields = ["amount"]
+
+    def clean_amount(self, *args, **kwargs):
+        print(args)
+        amount = self.cleaned_data.get("amount")
+        if amount <= 0:
+            raise forms.ValidationError("Please choose a positive amount")
+        if amount > self.product.stock:
+            raise forms.ValidationError("Not enough stock")
+        return amount
