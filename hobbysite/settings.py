7afeebcca254281ffc4b7fb9dd51a29e6077720a
Joseph Sagusay <joseph.sagusay@student.ateneo.edu>
2024-05-10 14:54:08 +0800

    Made the design for list and detail templates of blog.

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = "static/"
 
 MEDIA_ROOT = BASE_DIR / "media"
